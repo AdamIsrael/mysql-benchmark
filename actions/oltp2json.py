2cf0e7dcd31a6138f80f112b4ff32e413a7fa9fa
Adam Israel <adam@adamisrael.com>
2015-11-30 15:44:16 -0500

    charms.benchmark; Better logging of results; Percona support

--- actions/oltp2json.py
+++ actions/oltp2json.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env/python
-from charmbenchmark import Benchmark
+from charms.benchmark import Benchmark
 import re
 import sys
 
 
-def search(haystack, needle):
+def search(haystack, needle, filter=None):
     m = re.search(re.escape(needle) + r':\s+(.*)', haystack)
     if m:
+        if filter:
+            # treat filter as a regex to strip out extraneous data
+            m2 = re.search(
+                r'%s' % filter,
+                m.group(1).strip()
+            )
+            if m2:
+                return m2.group(0)
+
         return m.group(1).strip()
     return ""
 
@@ -16,28 +25,41 @@ def parse_sysbench_output():
 
     results = {}
 
-    results['read'] = search(output, 'read')
-    results['write'] = search(output, 'write')
-    results['other'] = search(output, 'other')
-    results['total'] = search(output, 'total')
-    results['transactions'] = search(output, 'transactions')
-    results['deadlocks'] = search(output, 'deadlocks')
-    results['rw.requests'] = search(output, 'read/write requests')
-    results['other.operations'] = search(output, 'other operations')
-    results['total.time'] = search(output, 'total time')
-    results['events'] = search(output, 'total number of events')
-    results['event.time'] = search(output, 'total time taken by event execution')
-    results['min'] = search(output, 'min')
-    results['avg'] = search(output, 'avg')
-    results['max'] = search(output, 'max')
-    results['95th'] = search(output, 'approx.  95 percentile')
-    results['avg.events'] = search(output, 'events (avg/stddev)')
-    results['avg.time'] = search(output, 'execution time (avg/stddev)')
+
+    results['read'] = {'value': search(output, 'read'), 'units': 'queries'}
+    results['write'] = {'value': search(output, 'write'), 'units': 'queries'}
+    results['other'] = {'value': search(output, 'other'), 'units': 'queries'}
+    results['total'] = {'value': search(output, 'total'), 'units': 'queries'}
+
+    results['transactions'] = {'value': search(output, 'transactions', '\d+'), 'units': ''}
+    # results['deadlocks'] = {'value': search(output, 'deadlocks'), 'units': ''}
+
+    results['rw-requests'] = {'value': search(output, 'read/write requests', '\d+'), 'units': ''}
+
+    results['other-operations'] = {'value': search(output, 'other operations', '\d+'), 'units': ''}
+    results['total-time'] = {'value': search(output, 'total time', '\d+\.\d+'), 'units': 'seconds'}
+    results['events'] = {'value': search(output, 'total number of events', '\d+'), 'units': 'events'}
+    results['event-time'] = {'value': search(output, 'total time taken by event execution', '\d+\.\d+'), 'units': 'seconds'}
+
+    results['min'] = {'value': search(output, 'min', '\d+\.\d+'), 'units': 'ms'}
+    results['avg'] = {'value': search(output, 'avg', '\d+\.\d+'), 'units': 'ms'}
+    results['max'] = {'value': search(output, 'max', '\d+\.\d+'), 'units': 'ms'}
+
+    results['95th'] = {'value': search(output, 'approx.  95 percentile', '\d+\.\d+'), 'units': 'ms'}
+    results['avg-time'] = {'value': search(output, 'execution time (avg/stddev)', '\d+\.\d+'), 'units': 'ms'}
+
+    # import json
+    # print json.dumps(results, sort_keys=True, indent=4, separators=(',', ': '))
+
+    # Benchmark.set_data({'results.transactions.value': 1096})
+    # Benchmark.set_data({'results.transactions.units': 'hits'})
 
     for key in results:
-        Benchmark.set_data({"results.%s" % key: results[key]})
+        # print {"results.%s.value" % key: results[key]['value']}
+        Benchmark.set_data({"results.%s.value" % key: results[key]['value']})
+        Benchmark.set_data({"results.%s.units" % key: results[key]['units']})
 
-    Benchmark.set_composite_score(results['95th'], '', 'asc')
+    Benchmark.set_composite_score(results['95th']['value'], 'ms', 'asc')
 
 if __name__ == "__main__":
     parse_sysbench_output()
